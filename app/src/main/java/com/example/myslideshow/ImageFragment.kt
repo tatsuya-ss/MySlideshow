baf10b86c40a0670fd596f88c26044c68c640a4d
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-01-17 11:26:30 +0900

    ImageFragmentの処理

--- app/src/main/java/com/example/myslideshow/ImageFragment.kt
+++ app/src/main/java/com/example/myslideshow/ImageFragment.kt
@@ -5,55 +5,60 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.myslideshow.databinding.FragmentImageBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val IMG_RES_ID = "IMG_RES_ID"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ImageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ImageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var imageResId: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 安全呼び出し演算子？とスコープ関数letを使ってnullじゃないことを確認
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            // 取り出したBundleオブジェクトはlet関数内ではitでアクセスできる
+            imageResId = it.getInt(IMG_RES_ID)
         }
     }
+
+    private var _binding: FragmentImageBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image, container, false)
+        _binding = FragmentImageBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        imageResId?.let {
+            binding.imageView.setImageResource(it)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    // コンパニオンオブジェクト = スタティックメソッド
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ImageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
+        // フラグメント内のImageViewに表示する画像のリソースIDを引数に取る
+        // フラグメントのインスタンスを生成して返す
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(imageResId: Int): ImageFragment =
             ImageFragment().apply {
+                // アーギュメントに保存するデータはバンドルクラスのインスタンス
+                // Bundle()はいくつかの値を「まとめる」クラス
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putInt(IMG_RES_ID, imageResId)
                 }
             }
-    }
+
+        }
 }
